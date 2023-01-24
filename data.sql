b0fb498acfa974e68878787aa35f4d04ee432d8a
SekoVper <damehlivingstone1@gmail.com>
2023-01-24 17:50:02 +0000

    Modify table data in db

--- data.sql
+++ data.sql
@@ -5,10 +5,10 @@ INSERT INTO animals VALUES (2, 'Gabumon', TO_DATE('15/11/2018', 'DD/MM/YYYY'), 2
 INSERT INTO animals VALUES (3, 'Pikachu' , TO_DATE('07/01/2021', 'DD/MM/YYYY'), 1, false, 15.04);
 INSERT INTO animals VALUES (4, 'Devimon' , TO_DATE('12/05/2017', 'DD/MM/YYYY'), 5, true, 11);
 
-INSERT INTO animals VALUES (5, 'Charmander' , TO_DATE('08/02/2020', 'DD/MM/YYYY'), 0, false, 11);
-INSERT INTO animals VALUES (6, 'Plantmon' , TO_DATE('15/11/2021', 'DD/MM/YYYY'), 2, true, 5.7);
-INSERT INTO animals VALUES (7, 'Squirtle' , TO_DATE('02/04/1993', 'DD/MM/YYYY'), 3, false, 12.13);
-INSERT INTO animals VALUES (8, 'Angemon' , TO_DATE('12/06/2005', 'DD/MM/YYYY'), 0, true, 45);
-INSERT INTO animals VALUES (9, 'Boarmon' , TO_DATE('07/06/2005', 'DD/MM/YYYY'), 7, true, 20.4);
-INSERT INTO animals VALUES (10, 'Blossom' , TO_DATE('13/10/1998', 'DD/MM/YYYY'), 3, true, 17);
-INSERT INTO animals VALUES (11, 'Ditto' , TO_DATE('14/05/2022', 'DD/MM/YYYY'), 4, true, 22);
+INSERT INTO animals VALUES (5, 'Charmander' , TO_DATE('08/02/2020', 'DD/MM/YYYY'), 0, false, -11);
+INSERT INTO animals VALUES (6, 'Plantmon' , TO_DATE('15/11/2021', 'DD/MM/YYYY'), 2, true, -5.7);
+INSERT INTO animals VALUES (7, 'Squirtle' , TO_DATE('02/04/1993', 'DD/MM/YYYY'), 3, false, -12.13);
+INSERT INTO animals VALUES (8, 'Angemon' , TO_DATE('12/06/2005', 'DD/MM/YYYY'), 0, true, -45);
+INSERT INTO animals VALUES (9, 'Boarmon' , TO_DATE('07/06/2005', 'DD/MM/YYYY'), 7, true, -20.4);
+INSERT INTO animals VALUES (10, 'Blossom' , TO_DATE('13/10/1998', 'DD/MM/YYYY'), 3, true, -17);
+INSERT INTO animals VALUES (11, 'Ditto' , TO_DATE('14/05/2022', 'DD/MM/YYYY'), 4, true, -22);
